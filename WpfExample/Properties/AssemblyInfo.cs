e103b636a858116ad305f4b1adfdb9758467ddcd
刘欢 <liuhuan@reyeah.com>
2025-12-24 12:26:15 +0800

    添加项目文件。

---
+++ WpfExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("EDSDKLib WpfExample Example")]
+[assembly: AssemblyDescription("WpfExample Example for  Canon SDK wrapper library")]
+[assembly: AssemblyProduct("WpfExample WinForms Example")]
+[assembly: AssemblyCopyright("Copyright © Johannes Bildstein 2016")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.2.1")]
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
